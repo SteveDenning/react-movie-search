f5174fb62c884a63de9c6f055c4d907458aa2149
Steve Denning <steve.denning@hotmail.co.uk>
2024-10-17 18:32:39 +0100

    003 - remove unused test

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+// Application wrapper
+import App from "./app";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
