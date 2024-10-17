629ab3392ef0372455b9ef847481a3b03ec3883f
Steve Denning <steve.denning@hotmail.co.uk>
2024-10-17 09:08:04 +0100

    004 - add mui to project

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Getting Started with Create React App
+# Getting Started
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Create a new environment variable file `cp .env.example .env`
 
 ## Available Scripts
 
@@ -44,4 +44,5 @@ You don’t have to ever use `eject`. The curated feature set is suitable for sm
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
 To learn React, check out the [React documentation](https://reactjs.org/).
+
 # react-movie-search
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
-function App() {
+import { AccessAlarm } from "@mui/icons-material";
+import Button from "./components/button";
+
+interface Props {}
+
+const App: React.FC<Props> = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello World</h1>
+        <Button>
+          <AccessAlarm sx={{ color: "red" }} />
+          Hello World
+        </Button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
