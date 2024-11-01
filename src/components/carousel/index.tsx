f64284429fd804be2978c7be66f551ea16f1a371
Steve Denning <steve.denning@hotmail.co.uk>
2024-11-01 10:58:33 +0000

    012 - update video and seperate carousel to new component

---
+++ src/components/carousel/index.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+
+import ReactCarousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import Image from "../../components/image";
+
+// Styles
+import "./carousel.scss";
+
+interface Props {
+  resources: any;
+}
+
+const Carousel: React.FC<Props> = ({ resources }) => {
+  const responsive = {
+    desktop: {
+      breakpoint: {
+        max: 3000,
+        min: 1024,
+      },
+      items: 5,
+      partialVisibilityGutter: -1,
+    },
+    tablet: {
+      breakpoint: {
+        max: 1024,
+        min: 464,
+      },
+      items: 3,
+      slidesToSlide: 1,
+      partialVisibilityGutter: 30,
+    },
+    mobile: {
+      breakpoint: {
+        max: 464,
+        min: 0,
+      },
+      items: 1,
+      slidesToSlide: 1,
+      partialVisibilityGutter: 30,
+    },
+  };
+
+  const options = {
+    renderDotsOutside: true,
+    responsive: responsive,
+    pauseOnHover: true,
+    slidesToSlide: 5,
+    infinite: true,
+    additionalTransfrom: 0,
+    arrows: true,
+    className: "",
+    containerClass: "container",
+    dotListClass: "",
+    draggable: true,
+    itemClass: "",
+    keyBoardControl: true,
+    minimumTouchDrag: 80,
+    partialVisible: true,
+    shouldResetAutoplay: true,
+    sliderClass: "",
+    showDots: false,
+    swipeable: true,
+  };
+
+  return (
+    <div className="carousel">
+      <ReactCarousel {...options}>
+        {resources.map((item: any, i: number) => {
+          return (
+            <div
+              key={i}
+              className="latest-releases__list-item"
+              onClick={() => (window.location.href = `/details/${item.id}`)}
+            >
+              <Image resource={item} />
+            </div>
+          );
+        })}
+      </ReactCarousel>
+    </div>
+  );
+};
+
+export default Carousel;
--- src/views/details/index.tsx
+++ src/views/details/index.tsx
@@ -6,15 +6,12 @@ import { Container, Fade } from "@mui/material";
 
 // Layout
 import DefaultLayout from "../../layout/default";
+import Video from "../../components/video";
 
 const DetailsView = () => {
   // const [details, setDetails] = useState<any>();
   // const [loaded, setLoaded] = useState<boolean>(false);
-  // const [trailer, setTrailer] = useState<string>("");
-
   // const programmeId = window.location.pathname.split("/")[2];
-
-  // const trailerUrl = "https://api-gate2.movieglu.com/trailers/?film_id=227902";
 
   return (
     <DefaultLayout heading={"TODO - set title"}>
@@ -22,30 +19,7 @@ const DetailsView = () => {
         <Fade in={true}>
           <div data-testid="details-page">
             <Button onClick={() => (window.location.href = "/")}>Back</Button>
-            {/* {trailer ? (
-              <video
-                controls
-                width="100%"
-                height="100%"
-                autoPlay
-              >
-                <source
-                  src={trailer}
-                  type="video/webm"
-                />
-                <source
-                  src={trailer}
-                  type="video/mp4"
-                />
-                Download the
-                <a href={trailer}>WEBM</a>
-                or
-                <a href={trailer}>MP4</a>
-                video.
-              </video>
-            ) : (
-              <p>Loading.....</p>
-            )} */}
+            <Video />
           </div>
         </Fade>
       </Container>
