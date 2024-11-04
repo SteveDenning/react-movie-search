34c5720af5477995d7522fe6d259c8dd14bad139
Steve Denning <steve.denning@hotmail.co.uk>
2024-11-04 10:38:11 +0000

    019 - add webpack to build

--- src/views/details/index.tsx
+++ src/views/details/index.tsx
@@ -8,44 +8,12 @@ import { Container, Fade } from "@mui/material";
 import DefaultLayout from "../../layout/default";
 
 const DetailsView = () => {
-  // const [details, setDetails] = useState<any>();
-  // const [loaded, setLoaded] = useState<boolean>(false);
-  // const [trailer, setTrailer] = useState<string>("");
-
-  // const programmeId = window.location.pathname.split("/")[2];
-
-  // const trailerUrl = "https://api-gate2.movieglu.com/trailers/?film_id=227902";
-
   return (
     <DefaultLayout heading={"TODO - set title"}>
       <Container>
         <Fade in={true}>
           <div data-testid="details-page">
             <Button onClick={() => (window.location.href = "/")}>Back</Button>
-            {/* {trailer ? (
-              <video
-                controls
-                width="100%"
-                height="100%"
-                autoPlay
-              >
-                <source
-                  src={trailer}
-                  type="video/webm"
-                />
-                <source
-                  src={trailer}
-                  type="video/mp4"
-                />
-                Download the
-                <a href={trailer}>WEBM</a>
-                or
-                <a href={trailer}>MP4</a>
-                video.
-              </video>
-            ) : (
-              <p>Loading.....</p>
-            )} */}
           </div>
         </Fade>
       </Container>
