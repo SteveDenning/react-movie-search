6b4403a392094d1512b8834169d4f7093c1cfe87
Steve Denning <steve.denning@hotmail.co.uk>
2024-12-12 15:43:58 +0000

    017 - update button tests

--- src/components/button/tests/button.test.tsx
+++ src/components/button/tests/button.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { screen, render } from "@testing-library/react";
+import { screen, render, fireEvent } from "@testing-library/react";
 
 // Components
 import Button from "../index";
@@ -10,11 +10,66 @@ describe("Button component", () => {
     const handleClick = jest.fn();
 
     beforeEach(() => {
-      render(<Button onClick={handleClick}>Lorem ipsum</Button>);
+      render(
+        <Button
+          onClick={handleClick}
+          className="custom-class"
+          variant="filled"
+          color="blue"
+        >
+          Click me
+        </Button>,
+      );
     });
 
     it("Should render a button", () => {
       expect(screen.getByTestId("button")).toBeInTheDocument();
+    });
+
+    it("Should render a button with a variant class of 'filled'", () => {
+      expect(screen.getByTestId("button")).toHaveClass("button--filled");
+    });
+
+    it("Should render a button with a color class of 'blue'", () => {
+      expect(screen.getByTestId("button")).toHaveClass("button--blue");
+    });
+
+    it("Should render a button with the text ", () => {
+      expect(screen.getByText("Click me")).toBeInTheDocument();
+    });
+
+    it("Should render a button with the a class of 'custom-class' ", () => {
+      expect(screen.getByTestId("button")).toHaveClass("custom-class");
+    });
+
+    it("Should allow the user to click the button", () => {
+      fireEvent.click(screen.getByTestId("button"));
+
+      expect(handleClick).toHaveBeenCalled();
+    });
+  });
+
+  describe("Component rendering (alternate state)", () => {
+    const handleClick = jest.fn();
+
+    beforeEach(() => {
+      render(
+        <Button
+          onClick={handleClick}
+          disabled
+          href="/"
+        >
+          Click me
+        </Button>,
+      );
+    });
+
+    it("Should disable the button", () => {
+      expect(screen.getByTestId("button")).toHaveAttribute("disabled");
+    });
+
+    it("Should render and anchor rather that a button element", () => {
+      expect(screen.getByTestId("button")).toHaveAttribute("href", "/");
     });
   });
 });
