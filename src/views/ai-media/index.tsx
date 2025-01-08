f1c6b815d0b087a6fc986df900e0ec702bf5a758
Steve Denning <steve.denning@hotmail.co.uk>
2025-01-08 12:27:12 +0000

    042 - update button styles and structure for results

--- src/views/ai-media/index.tsx
+++ src/views/ai-media/index.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 
 // Services
+import { getAllMediaFromSearch } from "../../services/search";
 import useOpenAI from "../../services/openai";
 
 // MUI Components
-import { Container } from "@mui/material";
+import { Container, Fade } from "@mui/material";
 
 // Components
 import AILoader from "../../components/ai-loader";
@@ -19,11 +20,17 @@ interface Props {
 
 const AIMedia: React.FC<Props> = () => {
   const [response, setResponse] = useState(null);
-  const [generating, setGenerating] = useState(false);
-  const [loaded, setLoaded] = useState(false);
+  const [generating, setGenerating] = useState<boolean>(false);
+  const [loaded, setLoaded] = useState<boolean>(false);
+  const [mediaType, setMediaType] = useState("tv");
+  const [linkType, setLinkType] = useState("tv");
+
+  const handleChange = (event, newAlignment: string) => {
+    setMediaType(newAlignment);
+  };
 
   const genres = "Comedy";
-  const prompt = `Give me a random list of 20 TV Shows that must have the following genres. ${genres}, these must be in an array of JSON objects called media, each object should have a name key with the name as the value`;
+  const prompt = `Give me a random list of 20 ${mediaType} that must have the following genres. ${genres}, these must be in an array of JSON objects called media, each object should have a name key with the name as the value`;
 
   const isJSONFormat = (obj) => {
     try {
@@ -42,6 +49,7 @@ const AIMedia: React.FC<Props> = () => {
         const resource = isJSONFormat(response.choices[0]?.message?.content);
 
         if (resource) {
+          setLinkType(mediaType);
           setResponse(resource);
           setGenerating(false);
           setLoaded(true);
@@ -59,6 +67,26 @@ const AIMedia: React.FC<Props> = () => {
       });
   };
 
+  const getMediaBySearchTerm = (query: string, linkType) => {
+    getAllMediaFromSearch(`${linkType}?query=${query}`)
+      .then((response: any) => {
+        if (response.data.results[0].id) {
+          window.location.href = `/details/${linkType}/${response.data.results[0].id}`;
+        } else {
+          alert("No results found");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  // Get both tv and movie data for favourites
+
+  // Loop through get all genre id then map again the list of genres to get the genre string
+
+  // Loop through the genre string and get the media based on the genre
+
   return (
     <div
       className="ai-media"
@@ -71,35 +99,58 @@ const AIMedia: React.FC<Props> = () => {
             data-testid="media-carousel-label"
             style={{ marginBottom: "20px" }}
           >
-            Let AI generate a list of programmes for you based of your favourite genres
+            Let AI generate a list of programmes for you based on your favourite genres
           </h2>
+          <div className="ai-media__toggle-wrapper">
+            <Button
+              onClick={(event) => handleChange(event, "tv")}
+              variant={mediaType === "tv" ? "filled" : "outlined"}
+              disabled={generating}
+            >
+              TV Shows
+            </Button>
+            <Button
+              onClick={(event) => handleChange(event, "movie")}
+              variant={mediaType === "movie" ? "filled" : "outlined"}
+              disabled={generating}
+            >
+              Movies
+            </Button>
+          </div>
+        </div>
+        <div style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
           {!generating && <Button onClick={getOpenAI}>Generate</Button>}
         </div>
         <div style={{ marginTop: "60px" }}>
           {generating ? (
             <AILoader />
-          ) : (
-            <ul className="ai-media__list">
-              {response?.media.length
-                ? response?.media.map((item, i) => {
+          ) : response?.media.length ? (
+            <Fade
+              in={response?.media.length}
+              timeout={1000}
+            >
+              <div>
+                <ul className="ai-media__list">
+                  {response?.media.map((item, i) => {
                     return (
                       <li
                         key={i}
                         className="ai-media__list-item"
                       >
-                        <Button> {item.name}</Button>
+                        <Button onClick={() => getMediaBySearchTerm(item.name, linkType)}> {item.name}</Button>
                       </li>
                     );
-                  })
-                : loaded && (
-                    <>
-                      <p>
-                        Oops! Looks like I had a little AI brain freeze ❄️. I promise it’s not because I was binge-watching cat videos in the
-                        background... Or was it? Let&#39;s try that again!
-                      </p>
-                    </>
-                  )}
-            </ul>
+                  })}
+                </ul>
+              </div>
+            </Fade>
+          ) : (
+            loaded && (
+              <p className="ai-media__error">
+                Oops! Looks like I had a little AI brain freeze ❄️. I promise it’s not because I was binge-watching cat videos in the background... Or
+                was it? Let&#39;s try that again!
+              </p>
+            )
           )}
         </div>
       </Container>
