76c71d321aae39cde3d7e4c100a3c14b2d355083
Steve Denning <steve.denning@hotmail.co.uk>
2025-01-31 19:12:20 +0000

    hotfix-01 - update request path

--- src/services/user.ts
+++ src/services/user.ts
@@ -6,7 +6,7 @@ import { headers } from "./headers";
 export const getRequestToken = async (): Promise<any> => {
   const environment = process.env.NODE_ENV;
   const redirectTo = environment === "development" ? "http://localhost:3000/" : "https://sd-react-movie-search.web.app/";
-  const url = `${process.env.REACT_APP_TMDB_ROOT}/4/auth/request_token`;
+  const url = "https://api.themoviedb.org/4/auth/request_token";
 
   const response: AxiosResponse<any> = await axios.post(
     url,
@@ -20,7 +20,7 @@ export const getRequestToken = async (): Promise<any> => {
 };
 
 export const getAccessToken = async (body: any): Promise<any> => {
-  const url = `${process.env.REACT_APP_TMDB_ROOT}/4/auth/access_token`;
+  const url = "https://api.themoviedb.org/4/auth/access_token";
 
   const response: AxiosResponse<any> = await axios.post(url, body, { ...headers, method: "POST" });
 
@@ -28,7 +28,7 @@ export const getAccessToken = async (body: any): Promise<any> => {
 };
 
 export const deleteAccessToken = async (accessToken: string): Promise<any> => {
-  const url = `${process.env.REACT_APP_TMDB_ROOT}/4/auth/access_token`;
+  const url = "https://api.themoviedb.org/4/auth/access_token";
 
   const response: AxiosResponse<any> = await axios.delete(url, {
     ...headers,
@@ -42,7 +42,7 @@ export const deleteAccessToken = async (accessToken: string): Promise<any> => {
 };
 
 export const getAccountDetails = async (sessionId: string): Promise<any> => {
-  const url = `${process.env.REACT_APP_TMDB_ROOT}/3/account?session_id=${sessionId}`;
+  const url = `https://api.themoviedb.org/3/account?session_id=${sessionId}`;
 
   const response: AxiosResponse<any> = await axios.get(url, headers);
 
@@ -50,7 +50,7 @@ export const getAccountDetails = async (sessionId: string): Promise<any> => {
 };
 
 export const createSessionWithAccessToken = async (body: any): Promise<any> => {
-  const url = `${process.env.REACT_APP_TMDB_ROOT}/3/authentication/session/convert/4`;
+  const url = "https://api.themoviedb.org/3/authentication/session/convert/4";
 
   const response: AxiosResponse<any> = await axios.post(url, body, { ...headers, method: "POST" });
 
