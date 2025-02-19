e9f00ff52cb6b592343fc4802173d3f4fa05384a
Steve Denning <steve.denning@hotmail.co.uk>
2025-02-19 09:35:19 +0000

    099 - hiding environment variables

--- src/services/headers.ts
+++ src/services/headers.ts
@@ -2,6 +2,6 @@ export const headers = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: process.env.REACT_APP_TMDB_AUTH_TOKEN,
+    Authorization: `Bearer ${process.env.REACT_APP_TMDB_AUTH_TOKEN}`,
   },
 };
