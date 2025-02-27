f767cdbfe4fad94dd2194d6143c721f2f6222a8e
Steve Denning <steve.denning@hotmail.co.uk>
2025-02-27 20:32:13 +0000

    027 - remove redundant test

--- src/services/favorites.ts
+++ src/services/favorites.ts
@@ -6,7 +6,7 @@ import { headers } from "./headers";
 
 export const getFavorites = async (user: any, type: string): Promise<any> => {
   if (user && type) {
-    const url = `https://api.themoviedb.org/3/account/${user.id}/favorite/${
+    const url = `https://api.themoviedb.org/3/account/${user.account_id}/favorite/${
       type === "movie" ? pluralize(type) : type
     }?language=en-US&page=1&sort_by=created_at.desc&session_id=${user.session_id}`;
     const response: AxiosResponse<any> = await axios.get(url, headers);
@@ -17,7 +17,7 @@ export const getFavorites = async (user: any, type: string): Promise<any> => {
 
 export const updateFavorite = async (user: any, body): Promise<any> => {
   if (user) {
-    const url = `https://api.themoviedb.org/3/account/${user}/favorite?session_id=${user.session_id}`;
+    const url = `https://api.themoviedb.org/3/account/${user.account_id}/favorite?session_id=${user.session_id}`;
     const response: AxiosResponse<any> = await axios.post(url, body, { ...headers, method: "POST" });
 
     return response;
