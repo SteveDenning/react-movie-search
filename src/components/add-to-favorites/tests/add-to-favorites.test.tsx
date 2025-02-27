acc8643e8d233797dac0a703353af48916ca9ac0
Steve Denning <steve.denning@hotmail.co.uk>
2025-02-27 18:23:00 +0000

    027 - fix add to favourite test

--- src/components/add-to-favorites/tests/add-to-favorites.test.tsx
+++ src/components/add-to-favorites/tests/add-to-favorites.test.tsx
@@ -13,30 +13,13 @@ let mockStorage = {};
 describe("Add to favorites component", () => {
   const handleFavorite = jest.fn();
 
-  beforeAll(() => {
-    global.Storage.prototype.setItem = jest.fn((key, value) => {
-      mockStorage[key] = value;
-    });
-    global.Storage.prototype.getItem = jest.fn((key) => mockStorage[key]);
-  });
-
-  afterAll(() => {
-    // @ts-ignore
-    global.Storage.prototype.setItem.mockReset();
-    // @ts-ignore
-    global.Storage.prototype.getItem.mockReset();
-  });
-
   describe("Component rendering (logged in)", () => {
     beforeEach(() => {
-      mockStorage = {
-        user: JSON.stringify(variables.user),
-      };
-
       render(
         <AddToFavorites
           isFavorite={false}
           handleFavorite={handleFavorite}
+          user
         />,
       );
     });
@@ -49,6 +32,23 @@ describe("Add to favorites component", () => {
     it("Should update the favourite icon when clicked", async () => {
       fireEvent.click(screen.getByTestId("add-to-favorites"));
       await waitFor(() => expect(screen.queryByTestId("FavoriteIcon")).toBeInTheDocument());
+    });
+  });
+
+  describe("Component rendering (logged out)", () => {
+    beforeEach(() => {
+      render(
+        <AddToFavorites
+          isFavorite={false}
+          handleFavorite={handleFavorite}
+          user={false}
+        />,
+      );
+    });
+
+    it("Should render an Add To Favorites button", () => {
+      expect(screen.getByTestId("add-to-favorites")).toBeInTheDocument();
+      expect(screen.getByTestId("FavoriteBorderIcon")).toBeInTheDocument();
     });
   });
 });
