4cf326d484b5688393dba27ade2da813f9405fdf
Steve Denning <steve.denning@cgi.com>
2025-06-10 15:37:50 +0100

    070 - update tests for home page and favourites

--- e2e/home-page.spec.ts
+++ e2e/home-page.spec.ts
@@ -2,22 +2,13 @@ import { test, expect } from "@playwright/test";
 import { login } from "./helpers/login";
 
 test.describe("Home Page (Logged out)", () => {
-  test("Page has correct title", async ({ page }) => {
+  test("Page has correct contents", async ({ page }) => {
     await page.goto("https://my-mdb.co.uk/");
     await expect(page).toHaveTitle("My Movie Database | Home");
     await expect(page).toHaveURL("https://my-mdb.co.uk/");
-  });
-
-  test("Page has correct header", async ({ page }) => {
-    await page.goto("https://my-mdb.co.uk/");
-
-    expect(await page.locator("header").textContent()).toContain("My Movie Database");
-  });
-
-  test("Page has section title of 'Trending this week'", async ({ page }) => {
-    await page.goto("https://my-mdb.co.uk/");
-
+    await expect(page.locator("footer")).toBeVisible();
     await expect(page.getByRole("heading", { name: "Trending this week" })).toBeVisible();
+    expect(await page.locator("header").textContent()).toContain("My Movie Database");
   });
 
   test("User is prompted to log in when clicking favourite button", async ({ page }) => {
@@ -30,12 +21,6 @@ test.describe("Home Page (Logged out)", () => {
     await page.getByTestId("modal").locator("div").nth(3).click();
     await page.getByTestId("modal-close-button").click();
     await expect(page.getByTestId("modal")).not.toBeVisible();
-  });
-
-  test("Page should contain a footer", async ({ page }) => {
-    await page.goto("https://my-mdb.co.uk/");
-
-    await expect(page.locator("footer")).toBeVisible();
   });
 });
 
