7cb9575179bf6e38790ea26da701ac7c722fdaed
Steve Denning <steve.denning@cgi.com>
2025-06-10 11:36:37 +0100

    070 - update tests for home page

--- e2e/home-page.spec.ts
+++ e2e/home-page.spec.ts
@@ -10,20 +10,32 @@ test.describe("Home Page (Logged out)", () => {
 
   test("Page has correct header", async ({ page }) => {
     await page.goto("https://my-mdb.co.uk/");
-    const header = await page.locator("header").textContent();
-    expect(header).toContain("My Movie Database");
+
+    expect(await page.locator("header").textContent()).toContain("My Movie Database");
+  });
+
+  test("Page has section title of 'Trending this week'", async ({ page }) => {
+    await page.goto("https://my-mdb.co.uk/");
+
+    await expect(page.getByRole("heading", { name: "Trending this week" })).toBeVisible();
   });
 
   test("User is prompted to log in when clicking favourite button", async ({ page }) => {
     await page.goto("https://my-mdb.co.uk/");
     await page.locator(".card > button:nth-child(3)").first().click();
+
     await expect(page.getByTestId("modal")).toBeVisible();
     await expect(page.locator(".add-to-favorites__login-message")).toContainText("You must login to use this feature");
 
     await page.getByTestId("modal").locator("div").nth(3).click();
     await page.getByTestId("modal-close-button").click();
-
     await expect(page.getByTestId("modal")).not.toBeVisible();
+  });
+
+  test("Page should contain a footer", async ({ page }) => {
+    await page.goto("https://my-mdb.co.uk/");
+
+    await expect(page.locator("footer")).toBeVisible();
   });
 });
 
@@ -35,12 +47,4 @@ test.describe("Home Page (Logged in)", () => {
     await expect(page).toHaveTitle("My Movie Database | Home");
     await expect(page.locator(".login__user--logged-in")).toBeVisible();
   });
-
-  // test("User can add a movie to favourites", async ({ page }) => {
-  //   await page.goto("https://my-mdb.co.uk/");
-  //   await page.locator(".card > button:nth-child(3)").first().click();
-  //   await expect(page.getByTestId("modal")).toBeVisible();
-  //   await expect(page.locator(".add-to-favorites__message")).toContainText("Added to your favourites");
-  //   await page.getByTestId("modal-close-button").click();
-  // });
 });
