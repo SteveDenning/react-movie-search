4143c36fcea6be1b3add244e858d4540783c1390
Steve Denning <steve.denning@cgi.com>
2025-06-10 10:05:14 +0100

    070 - add tests for home page

---
+++ e2e/favourites-page.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from "@playwright/test";
+import { login } from "./helpers/login";
+
+test.describe("Home Page (Logged out)", () => {
+  test("Page has correct title", async ({ page }) => {
+    await page.goto("https://my-mdb.co.uk/");
+    await expect(page).toHaveTitle("My Movie Database | Home");
+    await expect(page).toHaveURL("https://my-mdb.co.uk/");
+  });
+
+  test("Page has correct header", async ({ page }) => {
+    await page.goto("https://my-mdb.co.uk/");
+    const header = await page.locator("header").textContent();
+    expect(header).toContain("My Movie Database");
+  });
+
+  test("User is prompted to log in when clicking favourite button", async ({ page }) => {
+    await page.goto("https://my-mdb.co.uk/");
+    await page.locator(".card > button:nth-child(3)").first().click();
+    await expect(page.getByTestId("modal")).toBeVisible();
+    await expect(page.locator(".add-to-favorites__login-message")).toContainText("You must login to use this feature");
+
+    await page.getByTestId("modal").locator("div").nth(3).click();
+    await page.getByTestId("modal-close-button").click();
+
+    await expect(page.getByTestId("modal")).not.toBeVisible();
+  });
+});
---
+++ e2e/home-page.spec.ts
@@ -0,0 +1,46 @@
+import { test, expect } from "@playwright/test";
+import { login } from "./helpers/login";
+
+test.describe("Home Page (Logged out)", () => {
+  test("Page has correct title", async ({ page }) => {
+    await page.goto("https://my-mdb.co.uk/");
+    await expect(page).toHaveTitle("My Movie Database | Home");
+    await expect(page).toHaveURL("https://my-mdb.co.uk/");
+  });
+
+  test("Page has correct header", async ({ page }) => {
+    await page.goto("https://my-mdb.co.uk/");
+    const header = await page.locator("header").textContent();
+    expect(header).toContain("My Movie Database");
+  });
+
+  test("User is prompted to log in when clicking favourite button", async ({ page }) => {
+    await page.goto("https://my-mdb.co.uk/");
+    await page.locator(".card > button:nth-child(3)").first().click();
+    await expect(page.getByTestId("modal")).toBeVisible();
+    await expect(page.locator(".add-to-favorites__login-message")).toContainText("You must login to use this feature");
+
+    await page.getByTestId("modal").locator("div").nth(3).click();
+    await page.getByTestId("modal-close-button").click();
+
+    await expect(page.getByTestId("modal")).not.toBeVisible();
+  });
+});
+
+test.describe("Home Page (Logged in)", () => {
+  test("User can login to My MDB", async ({ page }) => {
+    await page.goto("https://my-mdb.co.uk/");
+    await login(page);
+    // Assertions
+    await expect(page).toHaveTitle("My Movie Database | Home");
+    await expect(page.locator(".login__user--logged-in")).toBeVisible();
+  });
+
+  // test("User can add a movie to favourites", async ({ page }) => {
+  //   await page.goto("https://my-mdb.co.uk/");
+  //   await page.locator(".card > button:nth-child(3)").first().click();
+  //   await expect(page.getByTestId("modal")).toBeVisible();
+  //   await expect(page.locator(".add-to-favorites__message")).toContainText("Added to your favourites");
+  //   await page.getByTestId("modal-close-button").click();
+  // });
+});
