e43a05ad5304a8ba4eacc5ef1ecb579b2ee7933f
Steve Denning <steve.denning@hotmail.co.uk>
2025-06-25 13:59:19 +0100

    070 - minor pr change

--- e2e/favourites-page.spec.ts
+++ e2e/favourites-page.spec.ts
@@ -13,18 +13,3 @@ test.describe("Favourites Page", () => {
     await expect(page.getByText("You currently have no favourite movies")).toBeVisible();
   });
 });
-
-// test.describe("Favourites Page - With Favourites", () => {
-//   test("User should be able to add/remove favourites", async ({ page }) => {
-//     await login(page);
-
-//     await expect(page.locator(".login__user--logged-in")).toBeVisible();
-
-//     await page.locator("div:nth-child(2) > div > .carousel__item > .card > button:nth-child(3)").first().click();
-//     await page.locator("div:nth-child(2) > div > .carousel__item > .card > button:nth-child(4)").first().click();
-//     await page.getByTestId("login").click();
-//     await page.getByRole("button", { name: "Favourites" }).click();
-
-//     await expect(page).toHaveTitle("Favourites");
-//   });
-// });
